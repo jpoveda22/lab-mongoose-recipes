a861951775cfc1013ddf132df791a10033617ebe
jpoveda22 <jpoveda226@gmail.com>
2020-03-05 10:40:23 -0500

    done

--- index.js
+++ index.js
@@ -11,3 +11,49 @@ mongoose
   })
   .then(x => console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`))
   .catch(err => console.error('Error connecting to mongo', err));
+
+
+const myRecipe = {
+  title: 'Greek Salad',
+  level: 'Easy Peasy',
+  ingredients: 'Tomatoes, Feta Cheese, Olives, Oil, Salt, Pepper',
+  cuisine: 'Greek',
+  dishType: 'Snack',
+  image: '',
+  duration: 40,
+  creator: 'Chef Jorge'
+};
+
+Recipe.create(myRecipe)
+  .then(myRecipe => console.log('The name of the recipe is:', myRecipe.title))
+  .catch(error =>
+    console.log('An error happened while adding a recipe:', error)
+  );
+
+Recipe.insertMany(data)
+  .then(data.forEach(recipe => console.log('The title of the recipe is:', recipe.title)))
+  .catch(error =>
+    console.log('An error happened while adding a recipe:', error)
+  );
+
+
+Recipe.updateOne({
+    title: 'Rigatoni alla Genovese'
+  }, {
+    duration: 100
+  })
+  .then(() => console.log('The recipe has been updated'))
+  .catch(error =>
+    console.log('An error happened while updating a recipe:', error)
+  );
+
+
+Recipe.deleteOne({
+  title: 'Carrot Cake'
+}).then(() => console.log('The recipe has been deleted')).catch(err =>
+  console.log('An error happened while deleting a recipe', err));
+
+
+mongoose.connection.close(function () {
+  console.log('Mongoose default connection closed');
+});
